168593025c679c1e92fc901b57e215e8345d6aee
alex <alexyakimchyk@gmail.com>
2021-04-08 15:54:26 +0300

    added setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+install_requires = ['requests==2.25.1', 'cryptography==3.4.7', 'aiohttp==3.7.4']
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='pc4store',
+    version='1.0.0',
+    description='Python lib for integration with pc4store payment system',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    install_requires=install_requires,
+    include_package_data=True,
+)
