6fe25ce3f22702dcacf019471d5d8c6d267f564d
Daniil Ivanov <d.ivanov@list.family>
2023-07-04 19:02:55 +0500

    v2: minor fix

--- pc4store/clients/sync.py
+++ pc4store/clients/sync.py
@@ -15,7 +15,4 @@ class Pc4StoreClient(BaseClient):
             method, path, auth=(self.store_id, self.store_key), json=json
         )
         data = response.text
-        # from devtools import debug
-        # debug(response.json())
-
         return self._parse_response(obj_loader, data)
